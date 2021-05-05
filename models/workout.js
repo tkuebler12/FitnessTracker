ccddd829f903c5075592f842d7a520535d687c17
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-05-05 19:17:58 -0400

    added models

--- models/workout.js
+++ models/workout.js
@@ -2,18 +2,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter the type of workout you want"
-    },
-    value: {
-        type: Number,
-        required: "Enter how many reps"
-    },
-    date: {
+    day: {
         type: Date,
         default: Date.now
+    },
+    exercies: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
     }
 });
 
