0cd426e12717af613df2fe907006803e8eb509b7
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-05-05 19:29:24 -0400

    deploy

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 const apiRoute = require("./public/api.js");
